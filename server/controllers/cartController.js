6b045fc61d642c6a08a662f3c6f99fb2bc02d81c
Destiny Jones <destinyjones@Destinys-MacBook.local>
2019-05-28 21:12:00 -0700

    complete

---
+++ server/controllers/cartController.js
@@ -0,0 +1,37 @@
+const swag = require("../models/swag");
+
+module.exports = {
+    add: (req, res ) => {
+        const { id } = req.params;
+        let { user } = req.session;
+
+        const index = user.cart.findIndex(swag => swag.id == id )
+
+        if ( index === -1 ){
+            const selectedSwag = swag.find(swag => swag.id == id )
+            user.cart.push(selectedSwag);
+            user.total += selectedSwag.price;
+        }
+
+        res.status(200).send(user);
+    },
+
+    delete: (req, res ) => {
+        const { id } = req.params; 
+        const  { user } = req.session;
+
+        const index = user.cart.findIndex(swag => swag.id == id);
+        const selectedSwag = swag.fing(swag => swag.id == id);
+        
+        if(index !== -1){
+            user.cart.splice(index, 1);
+            user.total -= selectedSwag.price;
+        }
+        res.status(200).send(user);
+
+    },
+
+    checkout: (req, res ) => {
+
+    }
+}
